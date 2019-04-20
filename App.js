b9e30f51ddaff3fad51e1fddf64a4395687ec188
Thomas Dürr <thomduerr@gmail.com>
2019-04-20 15:02:49 +0200

    code cleanup

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer, createBottomTabNavigator } from "react-navigation";
-
-class Screen1 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Screen 1</Text>
-      </View>
-    );
-  }
-}
-
-class Screen2 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Screen 2</Text>
-      </View>
-    );
-  }
-}
+import Home from "./js/screens/Home";
+import Settings from "./js/screens/Settings";
 
 const TabNavigator = createBottomTabNavigator({
-  "Screen 1": Screen1,
-  "Screen 2": Screen2
+  Home: Home,
+  Settings: Settings
 });
 
 export default createAppContainer(TabNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
