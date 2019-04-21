cf3fb737b5ee8a5feb2038654a8144c539801c8a
Thomas Dürr <thomduerr@gmail.com>
2019-04-21 23:13:22 +0200

    add some settings

--- js/screens/Settings.js
+++ js/screens/Settings.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SectionList, StyleSheet, Text, View } from 'react-native'
 import { Icon } from 'expo'
+
+const SectionItem = props => {
+  return <Text style={styles.item}>{props.name}</Text>
+}
+
+const SectionHeader = props => {
+  return <Text style={styles.section}>{props.title}</Text>
+}
 
 export class Settings extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Settings</Text>
+        <SectionList
+          sections={[
+            {
+              title: 'Version',
+              data: [{ key: '1', name: 'v1.0' }]
+            },
+            {
+              title: 'Impressum',
+              data: [{ key: '2', name: 'ACME Inc.' }, { key: '3', name: 'Copyright 2019' }]
+            }
+          ]}
+          renderItem={({ item }) => <SectionItem name={item.name} />}
+          renderSectionHeader={({ section }) => <SectionHeader title={section.title} />}
+        />
       </View>
     )
   }
@@ -20,7 +41,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: 40
+  },
+  section: {
+    backgroundColor: 'whitesmoke',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'lightgrey',
+    fontSize: 18,
+    padding: 5
+  },
+  item: {
+    color: 'dimgrey',
+    fontSize: 18,
+    padding: 5
   }
 })
